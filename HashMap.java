9dc1048ad0200a9f3529551578d4d73a12ad2c1d
MihirBRUH <86928490+MihirBRUH@users.noreply.github.com>
2024-02-03 19:57:48 +0530

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -1,7 +1,6 @@
  HashMap<Integer,Integer>hm=new HashMap();
-        for(int a[]:matches){
-            hm.put(a[0],0);
-            hm.put(a[1],hm.getOrDefault(a[1],0)+1);
+       for(int a:nums){
+            hm.put(a,hm.getOrDefault(a,0)+1);
         }
         for(Map.Entry<Integer,Integer>entry:hm.entrySet()){
         System.out.println(entry.getKey()+" "+entry.getValue());
